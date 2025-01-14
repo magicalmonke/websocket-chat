664d0114649107a2e3c3beab15e61bf64c874cda
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2025-01-14 14:29:55 +0100

    🐛 Set server to listen on port 3000 explicitly

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(3000, () => {
     console.log(`Server started on port ${server.address().port}`);
 });
